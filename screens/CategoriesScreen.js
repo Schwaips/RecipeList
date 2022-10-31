415d0fa7869c0a442e9799a61057bc7fc3646119
Charlie Bertrand <charlie.bertrand@live.com>
2022-10-31 14:44:35 +0100

    setup global interface for main screen and style

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,21 +1,24 @@
 import { FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
 import { CATEGORIES } from "../data/dummy-data";
 
-
-function renderCategoryItem() {
-  return 
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
 }
 
 function CategoriesScreen() {
-
-
   return (
     <FlatList
       data={CATEGORIES}
       renderItem={renderCategoryItem}
       keyExtractor={(item) => item.id}
+      numColumns={2}
     />
   );
 }
+
+
 
 export default CategoriesScreen;
